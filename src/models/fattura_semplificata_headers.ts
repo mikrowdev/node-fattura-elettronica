526c5db99cc6973c1d11cfefc17b28c98e6a0788
mikrowdev <vladyslav.elmasah.git@gmail.com>
2024-03-09 19:07:28 +0100

    chore: created regime fiscale model file

--- src/models/fattura_semplificata_headers.ts
+++ src/models/fattura_semplificata_headers.ts
@@ -1,68 +1,48 @@
-import { CommonsDatiTrasmissione, AnagraficaBase, IDFiscaleIVA, Sede, RappresentanteFiscaleBase, REA, IdentificativiFiscali, CommonHeaders } from "./commons_headers";
-import { BaseDatiAnagrafici } from "./fattura_header";
+import {
+  CommonsDatiTrasmissione,
+  AnagraficaBase,
+  IDFiscaleIVA,
+  Sede,
+  RappresentanteFiscaleBase,
+  REA,
+  IdentificativiFiscali,
+  CommonHeaders,
+} from './commons_headers';
+import { BaseDatiAnagrafici } from './fattura_header';
+import { RegimeFiscaleFatturaSemplificata } from './regime_fiscale';
 
 interface DatiTrasmissioneFatturaSemplificata extends CommonsDatiTrasmissione {
-       formatoTrasmissione: FormatoTrasmissioneFatturaSemplificata;
+  formatoTrasmissione: FormatoTrasmissioneFatturaSemplificata;
 }
 
-interface CedentePrestatoreFatturaSemplificata extends AltriIdentificativiFiscali {
-       idFiscaleIVA: IDFiscaleIVA;
-       codiceFiscale?: string;
-       iscrizioneREA?: REA;
-       regimeFiscale: RegimeFiscaleFatturaSemplificata;
+interface CedentePrestatoreFatturaSemplificata
+  extends AltriIdentificativiFiscali {
+  idFiscaleIVA: IDFiscaleIVA;
+  codiceFiscale?: string;
+  iscrizioneREA?: REA;
+  regimeFiscale: RegimeFiscaleFatturaSemplificata;
 }
 
 enum FormatoTrasmissioneFatturaSemplificata {
-       /**
-        * fattura semplificata
-        */
-       FSM10 = 'FSM10',
+  /**
+   * fattura semplificata
+   */
+  FSM10 = 'FSM10',
 }
 
 interface CessionarioCommittenteFatturaSemplificata {
-       identificativiFiscali: IdentificativiFiscali,
-       altriIdentificativi: AltriIdentificativiFiscali,
+  identificativiFiscali: IdentificativiFiscali;
+  altriIdentificativi: AltriIdentificativiFiscali;
 }
 
 interface AltriIdentificativiFiscali extends AnagraficaBase {
-       sede: Sede;
-       stabileOrganizzazione?: Sede;
-       rappresentanteFiscale?: RappresentanteFiscaleBase;
+  sede: Sede;
+  stabileOrganizzazione?: Sede;
+  rappresentanteFiscale?: RappresentanteFiscaleBase;
 }
-
-/**
- * Tipi di Regime Fiscale.
- */
-enum RegimeFiscaleFatturaSemplificata {
-       /**
-        * (Ordinario)
-        */
-       ORDINARIO = 'RF01',
-       /**
-        * (Contribuenti minimi (art.1, c.96-117, L. 244/07))
-        */
-       CONTRIBUENTI_MINIMI = 'RF02',
-       /**
-        * (Agricoltura e attività connesse e pesca (artt.34 e 34-bis, DPR 633/72))
-        */
-       AGRICOLTURA_E_ATTIVITA_CONNESSE_E_PESCA = 'RF04',
-       /**
-        * (Vendita sali e tabacchi (art.74, c.1, DPR. 633/72))
-        */
-       VENDITA_SALI_E_TABACCHI = 'RF05',
-       /**
-        * (Commercio fiammiferi (art.74, c.1, DPR 633/72))
-        */
-       COMMERCIO_FIAMMIFERI = 'RF06',
-       /**
-        * (Editoria (art.74, c.1, DPR 633/72))
-        */
-       EDITORIA = 'RF07',
-}
-
 
 interface FatturaElettronicaSemplificataHeader extends CommonHeaders {
-       datiTrasmissione: DatiTrasmissioneFatturaSemplificata;
-       cedentePrestatore: CedentePrestatoreFatturaSemplificata;
-       cessionarioCommittente: CessionarioCommittenteFatturaSemplificata;
+  datiTrasmissione: DatiTrasmissioneFatturaSemplificata;
+  cedentePrestatore: CedentePrestatoreFatturaSemplificata;
+  cessionarioCommittente: CessionarioCommittenteFatturaSemplificata;
 }
---
+++ src/models/regime_fiscale.ts
@@ -0,0 +1,107 @@
+/**
+ * Tipi di Regime Fiscale.
+ */
+export enum RegimeFiscaleFatturaSemplificata {
+  /**
+   * (Ordinario)
+   */
+  ORDINARIO = 'RF01',
+  /**
+   * (Contribuenti minimi (art.1, c.96-117, L. 244/07))
+   */
+  CONTRIBUENTI_MINIMI = 'RF02',
+  /**
+   * (Agricoltura e attività connesse e pesca (artt.34 e 34-bis, DPR 633/72))
+   */
+  AGRICOLTURA_E_ATTIVITA_CONNESSE_E_PESCA = 'RF04',
+  /**
+   * (Vendita sali e tabacchi (art.74, c.1, DPR. 633/72))
+   */
+  VENDITA_SALI_E_TABACCHI = 'RF05',
+  /**
+   * (Commercio fiammiferi (art.74, c.1, DPR 633/72))
+   */
+  COMMERCIO_FIAMMIFERI = 'RF06',
+  /**
+   * (Editoria (art.74, c.1, DPR 633/72))
+   */
+  EDITORIA = 'RF07',
+}
+
+/**
+ * Tipi di Regime Fiscale.
+ */
+export enum RegimeFiscaleFatturaOrdinaria {
+  /**
+   * (Ordinario)
+   */
+  ORDINARIO = 'RF01',
+  /**
+   * (Contribuenti minimi (art.1, c.96-117, L. 244/07))
+   */
+  CONTRIBUENTI_MINIMI = 'RF02',
+  /**
+   * (Agricoltura e attività connesse e pesca (artt.34 e 34-bis, DPR 633/72))
+   */
+  AGRICOLTURA_E_ATTIVITA_CONNESSE_E_PESCA = 'RF04',
+  /**
+   * (Vendita sali e tabacchi (art.74, c.1, DPR. 633/72))
+   */
+  VENDITA_SALI_E_TABACCHI = 'RF05',
+  /**
+   * (Commercio fiammiferi (art.74, c.1, DPR 633/72))
+   */
+  COMMERCIO_FIAMMIFERI = 'RF06',
+  /**
+   * (Editoria (art.74, c.1, DPR 633/72))
+   */
+  EDITORIA = 'RF07',
+  /**
+   * (Gestione servizi telefonia (art.74, c.1, DPR 633/72))
+   */
+  GESTIONE_SERVIZI_TELEFONIA = 'RF08',
+  /**
+   * (Rivendita documenti di trasporto pubblico e di sosta (art.74, c.1, DPR 633/72))
+   */
+  RIVENDITA_DOCUMENTI_DI_TRASPORTO_PUBBLICO_E_DI_SOSTA = 'RF09',
+  /**
+   * (Intrattenimenti, giochi e altre attività di cui alla tariffa allegata al DPR 640/72 (art.74, c.6, DPR 633/72))
+   */
+  INTRATTENIMENTI_GIOCHI_E_ALTRE_ATTIVITA_DI_CUI_ALLA_TARIFFA_ALLEGATA_AL_DPR_640_72 = 'RF10',
+  /**
+   * (Agenzie viaggi e turismo (art.74-ter, DPR 633/72))
+   */
+  AGENZIE_VIAGGI_E_TURISMO = 'RF11',
+  /**
+   * (Agriturismo (art.5, c.2, L. 413/91))
+   */
+  AGRITURISMO = 'RF12',
+  /**
+   * (Vendite a domicilio (art.25-bis, c.6, DPR 600/73))
+   */
+  VENDITE_A_DOMICILIO = 'RF13',
+  /**
+   * (Rivendita beni usati, oggetti d’arte, d’antiquariato o da collezione (art.36, DL 41/95))
+   */
+  RIVENDITA_BENI_USATI_OGGETTI_DARTE_DANTIQUARIATO_O_DA_COLLEZIONE = 'RF14',
+  /**
+   * (Agenzie di vendite all’asta di oggetti d’arte, antiquariato o da collezione (art.40-bis, DL 41/95))
+   */
+  AGENZIE_DI_VENDITE_ALLASTA_DI_OGGETTI_DARTE_ANTIQUARIATO_O_DA_COLLEZIONE = 'RF15',
+  /**
+   * (IVA per cassa P.A. (art.6, c.5, DPR 633/72))
+   */
+  IVA_PER_CASSA_P_A = 'RF16',
+  /**
+   * (IVA per cassa (art. 32-bis, DL 83/2012))
+   */
+  IVA_PER_CASSA = 'RF17',
+  /**
+   * (Altro)
+   */
+  ALTRO = 'RF18',
+  /**
+   * (Regime forfettario (art.1, c.54-89, L. 190/2014))
+   */
+  REGIME_FORFETTARIO = 'RF19',
+}
